50d0b5e449f8dba98e24268f715c777bb102f5cc
NhatMinhz <trinhhoangnhatminh123@gmail.com>
2025-03-08 23:02:41 +0700

    Update And Fix Errors

---
+++ BusinessObjects/NewsArticle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects;
+
+public partial class NewsArticle
+{
+    public string NewsArticleId { get; set; } = null!;
+
+    [Display(Name = "Title of News")]
+    public string? NewsTitle { get; set; }
+
+    [Display(Name = "Headline")]
+    public string Headline { get; set; } = null!;
+
+    [Display(Name = "Created Date")]
+    public DateTime? CreatedDate { get; set; }
+
+    [Display(Name = "Content of News")]
+    public string? NewsContent { get; set; }
+
+    [Display(Name = "Source of News")]
+    public string? NewsSource { get; set; }
+
+    [Display(Name = "Category")]
+    public short? CategoryId { get; set; }
+
+    [Display(Name = "Status")]
+    public bool NewsStatus { get; set; } = true;
+
+    [Display(Name = "Created By")]
+    public short? CreatedById { get; set; }
+
+    [Display(Name = "Updated By")]
+    public short? UpdatedById { get; set; }
+
+    [Display(Name = "Modified Date")]
+    public DateTime? ModifiedDate { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual SystemAccount? CreatedBy { get; set; }
+
+    public virtual ICollection<NewsTag> NewsTags { get; set; } = new List<NewsTag>();
+}
---
+++ BusinessObjects/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    [Display(Name = "Tag Name")]
+    public string? TagName { get; set; }
+
+    [Display(Name = "Note")]
+    public string? Note { get; set; }
+
+    public virtual ICollection<NewsTag> NewsTags { get; set; } = new List<NewsTag>();
+}
